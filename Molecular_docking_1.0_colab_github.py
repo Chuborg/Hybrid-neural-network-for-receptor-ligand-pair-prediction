d9576f44f06dcb0731b8e13afa8fb2862bfbcc7a
Chuborg <48642319+Chuborg@users.noreply.github.com>
2020-10-18 16:19:43 +0900

    Add files via upload

---
+++ Molecular_docking_1.0_colab_github.py
@@ -0,0 +1,189 @@
+# 91% accuracy
+from google.colab import drive # layout for google drive
+
+drive.mount('/content/drive') # mounting point for google drive
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+%matplotlib inline
+
+# Use GPU
+torch.cuda.is_available()
+device = torch.device("cuda:0")
+print(device)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on GPU")
+else:
+    device = torch.device("cpu")
+    print("Running on CPU")
+
+data = np.load("/content/drive/My Drive/Molecular docking/molecular_docking.npy", allow_pickle=True)
+"""
+data[i][0] - receptor
+data[i][1] - ligand
+data[i][2] - label
+ """
+
+# HYPERPARAMETERS
+RECEPTOR_DIM = 256
+LIGAND_DIM = 30
+LEARNING_RATE = 0.01
+MOMENTUM = 0.9
+BATCH_SIZE = 64
+EPOCHS = 60
+
+X_ligand = torch.Tensor([i[0] for i in data]).view(-1, LIGAND_DIM * LIGAND_DIM)
+X_receptor = torch.Tensor([i[1] for i in data]).view(-1, 1, RECEPTOR_DIM, RECEPTOR_DIM)
+y = torch.Tensor([i[2] for i in data])
+
+print("Receptors: ", len(X_receptor))
+print("Ligands: ", len(X_ligand))
+print("Labels: ", len(y))
+
+
+# HYBRID NET, FC
+class TwoInputsNet(nn.Module):
+    def __init__(self):
+        global RECEPTOR_DIM, LIGAND_DIM
+        super(TwoInputsNet, self).__init__()
+        # Set convolution layers for receptors
+        self.conv1 = nn.Conv2d(1, 128, 20)
+        self.conv2 = nn.Conv2d(128, 128, 5)
+        self.conv3 = nn.Conv2d(128, 256, 2)
+        self.pool1 = nn.MaxPool2d((10, 10))
+        self.pool2 = nn.MaxPool2d((5, 5))
+        
+        #Set fully connected layers for receptors
+        self.fc1 = nn.Linear(1024, 256) # Calculate input value first
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 64)
+        
+        # Set fully connected layers for ligands
+        self.fc10 = nn.Linear(LIGAND_DIM * LIGAND_DIM, 512) # Calculate input value first
+        self.fc11 = nn.Linear(512, 128)
+        self.fc12 = nn.Linear(128, 64)
+        self.fc13 = nn.Linear(64, 64)
+        
+        # After concatenation
+        self.fc20 = nn.Linear(128, 64)
+        self.fc21 = nn.Linear(64, 64)
+        self.fc22 = nn.Linear(64, 2)
+        #self.fc23 = nn.Linear(2, 2)
+
+    def forward(self, input1, input2): # (receptor - r, ligand - l)
+        # Receptors
+        r = F.relu(self.conv1(input1))
+        r = self.pool1(r)
+        r = F.relu(self.conv2(r))
+        r = self.pool2(r)
+        r = F.relu(self.conv3(r))
+                
+        r = r.view(r.size(0), -1)
+        r = F.relu(self.fc1(r))
+        r = F.relu(self.fc2(r))
+        r = F.relu(self.fc3(r))
+        r = F.relu(self.fc4(r))
+        
+        # Ligands        
+        l = F.relu(self.fc10(input2))
+        l = F.relu(self.fc11(l))
+        l = F.relu(self.fc12(l))
+        l = F.relu(self.fc13(l))
+        
+        # now we can reshape `c` and `f` to 2D and concatenate them
+        combined = torch.cat((r, l), dim=1)
+        combined = torch.cat((r.view(r.size(0), -1), l.view(l.size(0), -1)), dim=1)
+        out = F.relu(self.fc20(combined))
+        out = F.relu(self.fc21(out))
+        out = self.fc22(out)
+        
+        return F.softmax(out, dim=1)
+    
+net = TwoInputsNet().to(device)
+print(net)
+
+# Hybrid train
+# Define optimizer
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+loss_function = nn.MSELoss()
+
+VAL_PCT = 0.1
+val_size = int(len(X_receptor) * VAL_PCT)
+
+print("Validation size: ", val_size)
+
+# Define number of training and test samples
+train_X_receptor = X_receptor[:-val_size]
+train_X_ligand = X_ligand[:-val_size]
+train_y = y[:-val_size]
+
+test_X_receptor = X_receptor[-val_size:]
+test_X_ligand = X_ligand[-val_size:]
+test_y = y[-val_size:]
+
+print("Number of training receptors: ", len(train_X_receptor))
+print("Number of training ligands: ", len(train_X_ligand))
+print("Number of testing receptors: ", len(test_X_receptor))
+print("Number of testing ligands: ", len(test_X_ligand))
+
+
+def train(net):
+    global RECEPTOR_DIM, LIGAND_DIM, EPOCHS, BATCH_SIZE
+    print("begin the training")
+    losses = []
+    for epoch in range(EPOCHS):
+        for i in tqdm(range(0, len(train_X_receptor), BATCH_SIZE)):
+            batch_receptor_X = train_X_receptor[i: i + BATCH_SIZE].view(-1, 1, RECEPTOR_DIM, RECEPTOR_DIM)
+            batch_ligand_X = train_X_ligand[i: i + BATCH_SIZE].view(-1, LIGAND_DIM * LIGAND_DIM)
+            batch_y = train_y[i: i + BATCH_SIZE]
+            
+            batch_receptor_X, batch_ligand_X, batch_y = batch_receptor_X.to(device), batch_ligand_X.to(device), batch_y.to(device)
+            
+            net.zero_grad()
+            
+            outputs = net(batch_receptor_X, batch_ligand_X)
+            loss = loss_function(outputs, batch_y)
+            loss.backward()
+            optimizer.step()
+        
+        print("Epoch: ", epoch, "Loss: ", round(float(loss), 3))
+        losses.append(loss)
+    
+    print("Loss value: ", round(float(loss), 3))
+    plt.plot(losses)
+
+# Accuracy test
+def test_1(net):
+    global RECEPTOR_DIM, LIGAND_DIM
+    print("begin the test")
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for i in tqdm(range(len(test_X_receptor))):
+            real_class = torch.argmax(test_y[i]).to(device)
+            receptor = test_X_receptor[i].view(-1, 1, RECEPTOR_DIM, RECEPTOR_DIM)
+            ligand = test_X_ligand[i].view(-1, LIGAND_DIM * LIGAND_DIM)
+            
+            receptor, ligand = receptor.to(device), ligand.to(device)
+
+            net_out = net(receptor, ligand)[0]
+
+            predicted_class = torch.argmax(net_out)
+            if predicted_class == real_class:
+                correct += 1
+            total += 1
+
+    print("Accuracy on test_1 samples: ", round(correct / total, 3))
+    
+
+train(net)
+test_1(net)
---
+++ Molecular_docking_data_preparation_1.2.py
@@ -0,0 +1,112 @@
+# DATA PREPARATION
+
+import os
+import numpy as np
+from scipy.spatial.distance import squareform, pdist # needed for 3d case
+import cv2
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from Bio.PDB.PDBParser import PDBParser
+
+
+class DataPreparation:
+    RECEPTORS = "D:/Projects/Molecular docking (colab with Borodin)/DATA/RECEPTORS/"
+    LIGANDS = "D:/Projects/Molecular docking (colab with Borodin)/DATA/LIGANDS/"
+    ANNOTATIONS = "D:/Projects/Molecular docking (colab with Borodin)/DATA/BioLiP_2013-03-6.txt"
+
+    
+    NUMBER_OF_SAMPLES = 3000
+    
+    count_receptors = 0
+    count_ligands = 0
+    count_not_ligands = 0
+    
+    receptors = []
+    ligands = []
+    not_ligands = []
+    
+    training_data = []
+
+
+    def distance_matrix(self, file, interpolation_size, interpolation_type):
+        coords = []
+        parser = PDBParser()
+        structure = parser.get_structure("molecule", file.path)
+        for model in structure:
+            for chain in model:
+                for residue in chain:
+                    for atom in residue:
+                        coords.append(atom.coord)
+        coords_array = np.asarray(coords, dtype="float")
+        distance = squareform(pdist(coords_array, 'euclidean'))
+        resized = cv2.resize(distance, (interpolation_size,interpolation_size), interpolation=interpolation_type)
+
+        return resized
+    
+    def make_data(self):
+        count_rec = 0
+        count_lig = 0
+
+        receptor_names = []
+        ligand_names = []
+
+        with open(self.ANNOTATIONS) as fh: # Open file and read N-lines
+            cnt = 0
+            while cnt < self.NUMBER_OF_SAMPLES: # Set NUMBER_OF_SAMPLES or set True to read whole file
+                line = fh.readline()
+                tuple = line.split()
+                pdb_id = tuple[0]
+                chain = tuple[1]
+                ligand_id = tuple[4]
+                ligand_chain = tuple[5]
+                ligand_number = tuple[6]
+                receptor_names.append(pdb_id + chain + ".pdb")
+                count_rec += 1
+                ligand_names.append(pdb_id + "_" + ligand_id + "_" + ligand_chain + "_" + ligand_number + ".pdb")
+                count_lig += 1
+#                 print("Receptor: ", pdb_id + chain + ".pdb", "Ligand: ", pdb_id + "_" + ligand_id + "_" + ligand_chain + "_" + ligand_number + ".pdb")
+                cnt += 1
+        
+        print(f"There are {count_rec} receptors and {count_lig} ligands in annotations")
+        
+        for receptor_name in tqdm(receptor_names):
+            for receptor_file in os.scandir(self.RECEPTORS):
+                if receptor_name in receptor_file.name:
+#                     self.receptors.append(receptor_file.name)
+                    self.receptors.append(self.distance_matrix(receptor_file, 256, cv2.INTER_LINEAR))
+                    self.count_receptors += 1
+        
+        for ligand_name in tqdm(ligand_names):
+            for ligand_file in os.scandir(self.LIGANDS):
+                if ligand_name in ligand_file.name:
+#                     self.ligands.append(ligand_file.name)
+                    self.ligands.append(self.distance_matrix(ligand_file, 30, cv2.INTER_AREA))
+                    self.count_ligands += 1
+                    
+        print(f"Length of receptors {len(self.receptors)}, length of ligands {len(self.ligands)}")
+                    
+        # Add molecules that cannot interact with receptors
+
+        for ligand_name in tqdm(ligand_names):
+            for ligand_file in os.scandir(self.LIGANDS):
+                if ligand_name[:3] not in ligand_file.name and len(self.not_ligands) < len(self.receptors):
+#                     self.not_ligands.append(ligand_file.name)
+                    self.not_ligands.append(self.distance_matrix(ligand_file, 30, cv2.INTER_AREA)) # not_ligands
+                    self.count_not_ligands += 1
+        
+        print(f"Number of receptors {self.count_receptors}; number of ligands {self.count_ligands}, number of not_ligands {self.count_not_ligands}")
+        
+        # Join receptors and ligands, add label
+        for receptor, ligand, not_ligand in zip(self.receptors, self.ligands, self.not_ligands):
+            self.training_data.append([receptor, ligand, np.eye(2)[0]])
+            self.training_data.append([receptor, not_ligand, np.eye(2)[1]])
+            
+        np.random.shuffle(self.training_data)
+        print("Number of train data: ", len(self.training_data))
+        np.save("D:/Projects/Molecular docking (colab with Borodin)/DATA/training_data.npy", self.training_data)
+        
+#         print("DATA ", self.training_data)
+
+
+DataPreparation().make_data()
+print("DONE")
